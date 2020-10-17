8dcfed64ced8be674e6076ba6c88568b720905dc
Ben Heaps <bheaps99@gmail.com>
2020-10-17 15:20:31 -0700

    4th14

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -24,8 +24,8 @@ namespace MVClab.Controllers
 
         public IActionResult Index()
         {
-            string wwwroot = _env.ContentRootPath;
-            string newPath = Path.GetFullPath(Path.Combine(wwwroot,@"TextFiles/"));
+            string wwwroot = _env.WebRootPath;
+            string newPath = Path.GetFullPath("D:/home/site/TextFiles/");
             string[] fileswDir = Directory.GetFiles(newPath);
             TextFile[] files = new TextFile[fileswDir.Length];
 
@@ -43,7 +43,8 @@ namespace MVClab.Controllers
         public IActionResult content(string id) //content lower case on purpose, Content(string ..) conflicts
         {
             string wwwroot = _env.ContentRootPath;
-            string newPath = Path.GetFullPath(Path.Combine(wwwroot,@"TextFiles/",@id+".txt"));
+            //string newPath = Path.GetFullPath(Path.Combine(wwwroot,@"TextFiles/",@id+".txt"));
+            string newPath = Path.GetFullPath("D:/home/site/TextFiles/"+@id);
             string content = System.IO.File.ReadAllText(@newPath);
             string[] contentArray = {content, "a"}; //need to pass array
 
